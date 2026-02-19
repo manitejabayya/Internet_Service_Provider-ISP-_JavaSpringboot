2121a5678faa1aa0c1180096504722b2ef3d41a3
manitejabayyaa <maniteja.bayya369@gmail.com>
2026-02-19 09:51:45 +0530

    first commit

---
+++ src/main/java/com/ispautomation/isp/IspApplication.java
@@ -0,0 +1,13 @@
+package com.ispautomation.isp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ispautomation/isp/IspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispautomation.isp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
